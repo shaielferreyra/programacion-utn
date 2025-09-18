d31de87e2471c8a807b4938ec2060390cb3596f8
shaielferreyra <ferreyrashaiel03@gmail.com>
2025-09-18 18:41:03 -0300

    se realiza un footer simple

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./global1.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 Mi Página Web - Todos los derechos reservados</p>
+    </footer>
+  );
+}
+
+export default Footer;
