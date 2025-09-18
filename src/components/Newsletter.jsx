10540a4d1623621440a69a71fcdf61c03f15b316
shaielferreyra <ferreyrashaiel03@gmail.com>
2025-09-18 19:02:07 -0300

    se crea una bandeja de suscripcion

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./person3.css";
+
+function Newsletter() {
+  return (
+    <section className="newsletter">
+      <h2>Suscríbete </h2>
+      <input type="email" placeholder="Tu email" />
+      <button>Suscribirse</button>
+    </section>
+  );
+}
+
+export default Newsletter;
