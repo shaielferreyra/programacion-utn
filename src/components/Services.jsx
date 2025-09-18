224a0cabe2b5f66ac2a79b122ca9b00707cf6516
shaielferreyra <ferreyrashaiel03@gmail.com>
2025-09-18 18:59:51 -0300

    se crea el apartado de servicios

---
+++ src/components/Services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./global3.css";
+
+function Services() {
+  return (
+    <section id="services">
+      <h2>Servicios</h2>
+      <ul>
+        <li>Diseño Web</li>
+        <li>Programación</li>
+        <li>Soporte Técnico</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Services;
