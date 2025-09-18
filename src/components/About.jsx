2f21536f98cd6c9776935d9206a4e243ef41c084
rubenci04 <rea_04@hotmail.com.ar>
2025-09-18 19:08:26 -0300

    creacion about.jsx

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./person2.css";
+
+function About() {
+  return (
+    <section id="about">
+      <h2>Sobre Nosotros</h2>
+      <p>Somos un equipo de 3 personas aprendiendo React y haciendo esta página divertida y bonita.</p>
+    </section>
+  );
+}
+
+export default About;
